7d8a6512221ce118040c38ccbbf6a33358f892b1
devbobnwaka <nwakachibueze@gmail.com>
2023-05-09 19:15:59 +0100

    mvr done

--- posts/views.py
+++ posts/views.py
@@ -10,11 +10,17 @@ from .models import Post
 from .forms import (Visibility,)
 from accounts.permissions import (RedirectHomeIfNotUserMixin, UserIsOwnerMixin)
 
+
 # Create your views here.
 class PostCreateView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, CreateView):
     model = Post
     form_class = Visibility
     # success_url = reverse_lazy('post:post-detail')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['template_header'] = "Create Post"
+        return context
 
     def get_success_url(self):
         return reverse_lazy('post:post-detail', kwargs={'slug': self.object.slug})
@@ -24,9 +30,28 @@ class PostCreateView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, CreateView)
         kwargs['user'] = self.request.user
         return kwargs
 
+    # def form_valid(self, form):
+    #     form.instance.user = self.request.user
+    #     return super().form_valid(form)
     def form_valid(self, form):
         form.instance.user = self.request.user
+        if 'draft' in self.request.POST:
+            return self.draft(form)
+        elif 'publish' in self.request.POST:
+            return self.publish(form)
+
+    def draft(self, form):
+        post = form.save(commit=False)
+        post.is_published = False
+        post.save()
         return super().form_valid(form)
+
+    def publish(self, form):
+        post = form.save(commit=False)
+        post.is_published = True
+        post.save()
+        return super().form_valid(form)
+
 
 
 class PostUpdateView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, UserIsOwnerMixin, UpdateView):
@@ -36,6 +61,11 @@ class PostUpdateView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, UserIsOwner
     def get_success_url(self):
         return reverse_lazy('post:post-detail', kwargs={'slug': self.object.slug})
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['template_header'] = "Update Post"
+        return context
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['user'] = self.request.user
@@ -43,12 +73,28 @@ class PostUpdateView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, UserIsOwner
 
     def form_valid(self, form):
         form.instance.user = self.request.user
+        if 'draft' in self.request.POST:
+            return self.draft(form)
+        elif 'publish' in self.request.POST:
+            return self.publish(form)
+
+    def draft(self, form):
+        post = form.save(commit=False)
+        post.is_published = False
+        post.save()
+        return super().form_valid(form)
+
+    def publish(self, form):
+        post = form.save(commit=False)
+        post.is_published = True
+        post.save()
         return super().form_valid(form)
 
 
 class PostDetailView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, DetailView):
     model = Post
     context_object_name = 'post'
+
 
 
 class PostListView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, ListView):
@@ -59,8 +105,7 @@ class PostListView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, ListView):
         qs = super().get_queryset()
         user = self.request.user
         # visibility = Section.objects.filter(organization=user.member.organization)
-        return qs.filter(visibility=user.member.section)
-
+        return qs.filter(visibility=user.member.section, is_published=True)
 
 class UserPostListView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, ListView):
     model = Post
@@ -70,6 +115,7 @@ class UserPostListView(LoginRequiredMixin, RedirectHomeIfNotUserMixin, ListView)
         qs = super().get_queryset()
         user = self.request.user
         return qs.filter(user=user)
+
 
 class PostDeleteView(UserIsOwnerMixin, DeleteView):
     model = Post
